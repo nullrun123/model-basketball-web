68175daaf4731b435583b5ddd315524e5a6590ed
Run <165893187+nullrun123@users.noreply.github.com>
2026-04-22 20:23:48 +0700

    update text textturebas

--- src/components/VideoBas.tsx
+++ src/components/VideoBas.tsx
@@ -1,76 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import gsap from 'gsap';
+import { ScrollTrigger , SplitText} from 'gsap/all';
+import { useGSAP } from "@gsap/react";
 
+gsap.registerPlugin(ScrollTrigger,SplitText);
 function VideoBas() {
+
+  const containerRef = useRef(null);
+  useGSAP(()=>{
+    
+    const tl = gsap.timeline({
+      scrollTrigger:{
+        trigger:containerRef.current,
+        start:"top top",
+        end:"+=200%",
+        pin:true,
+        scrub:2,
+      }
+    })
+  })
   return (
-//     <div className='w-screen h-screen relative bg-pink-100 flex-center'>
-//       <div className='flex-center w-full min-h-full'> 
-//       <div className='w-100'>
-//           <img className='object-cover ' src="Excel-bas.png" alt="" />
-//       </div>
-//         <div className='absolute w-full h-full  '>
 
-            // {/* <svg viewBox="-100 -100 150 150"
-            // className='abs-center border-2 w-screen h-full'
-            // width="100%" 
-            // height="100%"
-            // preserveAspectRatio="xMidYMid meet">
-            // <path
-            // d="M8.2322 10.9602 29 32Q32 33 31 30 25.436 25.334 9.7592 9.6559M5.3614 6.6751 9.9293 8.3297 7.016 11.243 5.3614 6.6751ZM6.5635 12.2895C6.6059 12.3319 6.6766 12.3744 6.7332 12.4027 6.8746 12.4309 7.0302 12.3885 7.1291 12.2895L10.9617 8.457C11.0607 8.358 11.1031 8.2024 11.0748 8.061S10.9334 7.8065 10.8061 7.764L4.7816 5.5579C4.6401 5.5013 4.4563 5.5437 4.3431 5.6569S4.1876 5.9538 4.2442 6.0953L6.4645 12.134C6.4928 12.1905 6.521 12.2471 6.5635 12.2895Z"
-            // stroke="#000"
-            // strokeWidth="0.5"
-            // fill="#000"
-            // />
-            // </svg> */}
-
-            // {/* <svg viewBox="-50 -100 150 150"
-            // className='abs-center border-2 w-screen h-full'
-            // width="100%" 
-            // height="100%"
-            // preserveAspectRatio="xMidYMid meet">
-            // <path
-            // d="M-10.9602 8.2322-32 29Q-33 32-30 31-25.334 25.436-9.6559 9.7592M-6.6751 5.3614-8.3297 9.9293-11.243 7.016-6.6751 5.3614ZM-12.2895 6.5635C-12.3319 6.6059-12.3744 6.6766-12.4027 6.7332-12.4309 6.8746-12.3885 7.0302-12.2895 7.1291L-8.457 10.9617C-8.358 11.0607-8.2024 11.1031-8.061 11.0748S-7.8065 10.9334-7.764 10.8061L-5.5579 4.7816C-5.5013 4.6401-5.5437 4.4563-5.6569 4.3431S-5.9538 4.1876-6.0953 4.2442L-12.134 6.4645C-12.1905 6.4928-12.2471 6.521-12.2895 6.5635Z"
-            // stroke="#000"
-            // strokeWidth="0.5"
-            // fill="#000"
-            // />
-            // </svg>
-
-            // <svg viewBox="-50 -5 150 150"
-            // className='abs-center border-2 w-screen h-full'
-            // width="100%" 
-            // height="100%"
-            // preserveAspectRatio="xMidYMid meet">
-            // <path
-            // d="M-8.2322 34.0398-29 13Q-32 12-31 15-25.436 19.666-9.7592 35.3441M-5.3614 38.3249-9.9293 36.6703-7.016 33.757-5.3614 38.3249ZM-6.5635 32.7105C-6.6059 32.6681-6.6766 32.6256-6.7332 32.5973-6.8746 32.5691-7.0302 32.6115-7.1291 32.7105L-10.9617 36.543C-11.0607 36.642-11.1031 36.7976-11.0748 36.939S-10.9334 37.1935-10.8061 37.236L-4.7816 39.4421C-4.6401 39.4987-4.4563 39.4563-4.3431 39.3431S-4.1876 39.0462-4.2442 38.9047L-6.4645 32.866C-6.4928 32.8095-6.521 32.7529-6.5635 32.7105Z"
-            // stroke="#000"
-            // strokeWidth="0.5"
-            // fill="#000"
-            // />
-            // </svg>
-
-
-
-            // <svg viewBox="-145 -10 150 150"
-            // className='abs-center border-2 w-screen h-full'
-            // width="100%" 
-            // height="100%"
-            // preserveAspectRatio="xMidYMid meet">
-            // <path
-            // d="M-32.7105 33.4365c-.0424.0425-.099.0707-.1555.099l-6.0387 2.2203c-.1415.0566-.3252.0143-.4384-.0989s-.1556-.297-.099-.4385l2.2061-6.0245c.0425-.1273.1556-.2404.297-.2687s.297.0141.396.1131l3.8325 3.8326c.099.0989.1414.2545.1132.3959-.0283.0566-.0708.1273-.1132.1697Zm-5.6144 1.2021L-33.757 32.984-36.6703 30.0707l-1.6546 4.5679Zm2.9808-4.3978Q-19.666 14.564-15 9q3-1 2 2L-34.0398 31.7678"
-            // stroke="#000"
-            // strokeWidth="0.5"
-            // fill="#000"
-            // />
-            // </svg> */}
-//     </div>
-//          </div>
-//     </div>
-
-<div className='w-screen h-screen bg-pink-100 flex items-center justify-center'>
-  <div className='relative w-150'>
+<div ref={containerRef} className='w-screen h-screen bg-white flex items-center justify-center'>
+  <div className='relative w-120 p-8'>
     <img className='w-full h-auto block' src="Excel-bas.png" alt="" />
 
-        <svg viewBox="-130 -100 150 150"
+        {/* BOTTOM */}
+           <svg viewBox="-130 -100 150 150"
             className='abs-center border-2 w-screen h-full'
             width="100%" 
             height="100%"
@@ -81,8 +37,20 @@ function VideoBas() {
             strokeWidth="0.5"
             fill="#000"
             />
-            </svg> 
-
+            <text 
+              x="37"       
+              y="10"       
+              fontSize="10"
+              fontWeight="bold"
+              fill="#000"
+              textAnchor="start">
+              Butyl Bladder
+            </text>
+        </svg>
+             <p className="absolute bottom-15 left-135 text-xl text-gray-500 w-60" >
+              Inner butyl rubber bladder retains air pressure longer.
+            </p>
+        
              <svg viewBox="-20 -100 150 150"
             className='abs-center border-2 w-screen h-full'
             width="100%" 
@@ -94,8 +62,22 @@ function VideoBas() {
             strokeWidth="0.5"
             fill="#000"
             />
+              <text 
+              x="-70%"       
+              y="10"       
+              fontSize="10"
+              fontWeight="bold"
+              fill="#000"
+              textAnchor="start">
+              Nylon Wounds
+            </text>
             </svg>
+             <p className="absolute bottom-8 right-135 text-xl text-gray-500 w-60" >
+              Multi-layer nylon winding maintains a perfect round shape.
+            </p>
+        
 
+        {/* TOP */}
             <svg viewBox="-20 -5 150 150"
             className='abs-center border-2 w-screen h-full'
             width="100%" 
@@ -107,7 +89,20 @@ function VideoBas() {
             strokeWidth="0.5"
             fill="#000"
             />
+              <text 
+              x="-70%"       
+              y="10"       
+              fontSize="10"
+              fontWeight="bold"
+              fill="#000"
+              textAnchor="start">
+             Deep Channels
+            </text>
             </svg>
+
+             <p className="absolute top-15 right-130 text-xl text-gray-500 w-60" >
+              Deep black seams between panels , improving passing and dribbling.
+            </p>
 
 
 
@@ -122,7 +117,21 @@ function VideoBas() {
             strokeWidth="0.5"
             fill="#000"
             />
+              <text 
+              x="-8"       
+              y="10"       
+              fontSize="10"
+              fontWeight="bold"
+              fill="#000"
+              textAnchor="start">
+              Pebbled Surface
+            </text>
             </svg> 
+            
+             <p className="absolute top-19 left-135 text-xl text-gray-500 w-65" >
+              Fine raised bumps enhance grip and ball control.
+            </p>
+
   </div>
 </div>
   )
